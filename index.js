fd971c97bf498f018e706ab705f50d4a5b7d386c
Jon Pena <iamjonntech@gmail.com>
2022-08-29 20:19:28 -0700

    adding database

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
-const port = process.env.PORT || 4000
-require('dotenv').config();
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+const todoRoutes = require('./routes/todos')
 
+require('dotenv').config({path: './config/.env'})
 
-// let dbConnectionStr = process.env.DB_STRING,
-//     dbName = 'trip-planner';
+// Passport config
+require('./config/passport')(passport)
 
-// MongoClient.connect(dbConnectionStr, {useUnifedTopology: true})
-//     .then(client => {
-//         console.log(`Connected to ${dbName} database`);
-//         db = client.db(dbName);
-//     })
-MongoClient.connect(process.env.DB_STRING, { useUnifiedTopology: true })
-    .then(client => {
-    console.log('Connected to Database Jon')
-    
-//     const db = client.db('star-wars-quotes')
-    //const tripsCollection = db.collection('trips')
-    
+connectDB()
 
-    // app.set('view engine', 'ejs');    
-    // app.use(cors());
-    // app.use(express.static('public'));
-    // app.use(express.urlencoded({extended: true}));
-    // app.use(express.json());
-
-
-    // app.get('/', (req, res) => {
-    //     db.collection('travels').find().toArray()
-    //         .then(travels => {
-    //             res.render('index.ejs', { info: data })
-    //         })
-    //         .catch(err => console.log(err))
-    // })
-
-    // app.post('/addTrip', (req, res) => {
-    //     //<TODO>add a confirmed button to the form</TODO>
-    //     db.collection('travels').insertOne({hotelName: req.body.hotelName, hotelPrice: req.body.hotelPrice, hotelDate: req.body.hotelDate})
-    // })
-
-    app.listen(port, () => {
-        console.log(`the server is running on ${port}!`)
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev'))
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
     })
-})
-.catch(err => console.log(err))
+  )
+  
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
+  
+app.use('/', mainRoutes)
+app.use('/todos', todoRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+}) 
