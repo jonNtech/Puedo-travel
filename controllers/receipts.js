8fef010d89a8e74053e7fab56012888409a13292
Jon Pena <iamjonntech@gmail.com>
2022-08-30 17:05:26 -0700

    fixed Mongo error

--- controllers/receipts.js
+++ controllers/receipts.js
@@ -11,7 +11,7 @@ module.exports = {
             console.log(err)
         }
     },
-    createTodo: async (req, res)=>{
+    createReceipt: async (req, res)=>{
         try{
             await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
             console.log('Todo has been added!')
@@ -20,7 +20,7 @@ module.exports = {
             console.log(err)
         }
     },
-    markComplete: async (req, res)=>{
+    markApproved: async (req, res)=>{
         try{
             await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
                 completed: true
@@ -31,7 +31,7 @@ module.exports = {
             console.log(err)
         }
     },
-    markIncomplete: async (req, res)=>{
+    markDeclined: async (req, res)=>{
         try{
             await Todo.findOneAndUpdate({_id:req.body.todoIdFromJSFile},{
                 completed: false
@@ -42,7 +42,7 @@ module.exports = {
             console.log(err)
         }
     },
-    deleteTodo: async (req, res)=>{
+    deleteReceipt: async (req, res)=>{
         console.log(req.body.todoIdFromJSFile)
         try{
             await Todo.findOneAndDelete({_id:req.body.todoIdFromJSFile})
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-// const MongoStore = require('connect-mongo')(session)
+const MongoStore = require('connect-mongo')(session)
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
@@ -13,7 +13,7 @@ const receiptRoutes = require('./routes/receipts')
 require('dotenv').config({path: './config/.env'})
 
 // Passport config
-// require('./config/passport')(passport)
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -21,25 +21,25 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-// app.use(logger('dev'))
+app.use(logger('dev'))
 // Sessions
-// app.use(
-//     session({
-//       secret: 'keyboard cat',
-//       resave: false,
-//       saveUninitialized: false,
-//       store: new MongoStore({ mongooseConnection: mongoose.connection }),
-//     })
-//   )
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
   
 // Passport middleware
-// app.use(passport.initialize())
-// app.use(passport.session())
+app.use(passport.initialize())
+app.use(passport.session())
 
-// app.use(flash())
+app.use(flash())
   
-// app.use('/', mainRoutes)
-// app.use('/receipts', receiptRoutes)
+app.use('/', mainRoutes)
+app.use('/receipts', receiptRoutes)
  
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
