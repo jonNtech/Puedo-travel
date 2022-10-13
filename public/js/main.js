36df11c893ebf428e3953272efb400a59ffe2713
Jon Pena <iamjonntech@gmail.com>
2022-10-12 21:35:09 -0700

    worked on styling

--- public/js/main.js
+++ public/js/main.js
@@ -45,20 +45,53 @@ async function markNotApproved(){
     }
 }
 
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("googleMap"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-}
+// // Initialize and add the map
+// function initMap() {
+//     // The location of Uluru
+//     const uluru = { lat: -25.344, lng: 131.031 };
+//     // The map, centered at Uluru
+//     const map = new google.maps.Map(document.getElementById("googleMap"), {
+//       zoom: 4,
+//       center: uluru,
+//     });
+//     // The marker, positioned at Uluru
+//     const marker = new google.maps.Marker({
+//       position: uluru,
+//       map: map,
+//     });
+// }
   
-  window.initMap = initMap;
+//   window.initMap = initMap;
+
+// let autocomplete  
+// function initAutocomplete() {
+//     autocomplete = new google.maps.places.Autocomplete(
+//         document.getElementById("autocomplete"),
+//         {
+//             types: ['establishment'],
+//             componentRestrictions: {"country" : ['US']},
+//             fields: ['place_id', 'geomtry', 'name']
+//         }
+//     )
+//     autocomplete.addListener('place_changed', onPlaceChanged)
+// }
+
+// function onPlaceChanged(){
+//     let place = autocomplete.getPlace()
+
+//     if(!place.geometry){
+//         document.getElementById('autocomplete').placeholder = 'Enter a place'
+//     } else (
+//         document.getElementById('details'). innerHTML = place.name,
+//         console.log(place)
+//     )
+// }
+
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', ()=> {
+    btn.classList.toggle('open')
+    nav.classList.toggle('flex')
+    nav.classList.toggle('hidden')
+})
